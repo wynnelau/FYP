c7cc12083dcf65d2f2e272bf6f5452a5b4c22e0a
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2023-09-01 16:16:25 +0800

    Subbed in RegisterLoginChange
    change from LoginHerePressed and RegisterHerePressed to RegisterLoginChange

--- My project/Assets/Scripts/RegisterLogin/RegisterLoginChange.cs
+++ My project/Assets/Scripts/RegisterLogin/RegisterLoginChange.cs
@@ -1,27 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
 
 public class RegisterLoginChange : MonoBehaviour
 {
+
     public GameObject registerPage;
     public GameObject loginPage;
-
+    public void ChangeToRegister()
+    {
+        loginPage.SetActive(false);
+        registerPage.SetActive(true);
+    }
+    public void ChangeToLogin()
+    {
+        loginPage.SetActive(true);
+        registerPage.SetActive(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        registerPage.SetActive(false);
-        loginPage.SetActive(true);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-    
 }
--- My project/Assets/Scripts/RegisterLogin/RegisterLoginInitialise.cs
+++ My project/Assets/Scripts/RegisterLogin/RegisterLoginInitialise.cs
@@ -1,25 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class RegisterHerePressed : MonoBehaviour
+
+public class RegisterLoginInitialise : MonoBehaviour
 {
     public GameObject registerPage;
     public GameObject loginPage;
-    public void ChangeToRegister()
-    {
-        loginPage.SetActive(false);
-        registerPage.SetActive(true);
-    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        registerPage.SetActive(false);
+        loginPage.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    
 }
