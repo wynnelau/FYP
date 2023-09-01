fffcb202459cb26bd3fcb62f00d4050c090690d2
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2023-09-01 15:02:17 +0800

    update login and register page

---
+++ My project/Assets/Scripts/RegisterLogin/RegisterLoginChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class RegisterLoginChange : MonoBehaviour
+{
+    public GameObject registerPage;
+    public GameObject loginPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        registerPage.SetActive(false);
+        loginPage.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
