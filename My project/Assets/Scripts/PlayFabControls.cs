92d59c000b64a7228c3e393e735bee5c95e4f76a
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2023-09-01 17:23:25 +0800

    connect to cloud

--- My project/Assets/Scripts/PlayFabControls.cs
+++ My project/Assets/Scripts/PlayFabControls.cs
@@ -5,23 +5,84 @@ using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
 using TMPro;
+using UnityEditor.PackageManager;
 
 public class PlayFabControls : MonoBehaviour
 {
     [SerializeField] GameObject registerPage, loginPage;
     public TMP_Text registerEmail, registerPassword, registerError, loginEmail, loginPassword, loginError;
+    public Text registerUsernameLegacy;
     private string encryptedPassword;
 
     public void ChangeToLogin()
     {
         loginPage.SetActive(true);
         registerPage.SetActive(false);
+        loginError.text = " ";
+        registerError.text = " ";
     }
 
     public void ChangeToRegister()
     {
         loginPage.SetActive(false);
         registerPage.SetActive(true);
+        loginError.text = " ";
+        registerError.text = " ";
+    }
+
+    string Encrypt(string pass)
+    {
+        System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+        byte[] bs = System.Text.Encoding.UTF8.GetBytes(pass);
+        bs = x.ComputeHash(bs);
+        System.Text.StringBuilder s = new System.Text.StringBuilder();
+        foreach (byte b in bs)
+        {
+            s.Append(b.ToString("x2").ToLower());
+        }
+        return s.ToString();
+    }
+
+    public void Register()
+    {
+        var registerRequest = new RegisterPlayFabUserRequest { Email = registerEmail.text, Password = Encrypt(registerPassword.text), Username = registerUsernameLegacy.text };
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, RegisterSuccess, RegisterFail);
+    }
+
+    public void RegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        loginError.text = " ";
+        registerError.text = " ";
+        StartGame();
+    }
+
+    public void RegisterFail(PlayFabError error)
+    {
+        registerError.text = error.GenerateErrorReport();
+    }
+
+    public void LoginSuccess(LoginResult result)
+    {
+        loginError.text = " ";
+        registerError.text = " ";
+        StartGame();
+    }
+
+    public void LoginFail(PlayFabError error)
+    {
+        loginError.text = error.GenerateErrorReport();
+    }
+
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest { Email = loginEmail.text, Password = Encrypt(loginPassword.text) };
+        PlayFabClientAPI.LoginWithEmailAddress(request, LoginSuccess, LoginFail);
+    }
+
+    void StartGame()
+    {
+        loginPage.SetActive(false);
+        registerPage.SetActive(false);
     }
     
 }
