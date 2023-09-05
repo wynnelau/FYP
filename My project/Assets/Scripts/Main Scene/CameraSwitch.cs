693fba36dae630be53e266b0be33c928c3df02cd
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2023-09-05 11:27:10 +0800

    added boundary to firstpersonview

--- My project/Assets/Scripts/Main Scene/CameraSwitch.cs
+++ My project/Assets/Scripts/Main Scene/CameraSwitch.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
 
 public class CameraSwitch : MonoBehaviour
 {
     public Camera firstPersonCamera;
     public Camera thirdPersonCamera;
-    public GameObject buildingFirstPerson;
+    public GameObject buildingFirstPerson, middleWall;
+    public PlayerControls player;
     // Start is called before the first frame update
 
     void SwitchToFirstPerson()
@@ -14,7 +13,7 @@ public class CameraSwitch : MonoBehaviour
         firstPersonCamera.enabled = true;
         thirdPersonCamera.enabled = false;
         buildingFirstPerson.SetActive(true);
-
+        middleWall.SetActive(false);
     }
 
     void SwitchToThirdPerson()
@@ -22,6 +21,9 @@ public class CameraSwitch : MonoBehaviour
         firstPersonCamera.enabled = false;
         thirdPersonCamera.enabled = true;
         buildingFirstPerson.SetActive(false);
+        middleWall.SetActive(true);
+        player.BottomLimit();
+
     }
     void Start()
     {
--- My project/Assets/Scripts/Main Scene/PlayerControls.cs
+++ My project/Assets/Scripts/Main Scene/PlayerControls.cs
@@ -5,12 +5,8 @@ public class PlayerControls : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     private float speed = 10.0f;
-    private float xRange = 23;
-    private float leftBoundary = -7;
-    private float rightBoundary = 22;
     private Vector3 offset;
-    
-
+    private float firstPersonBoundary = -22.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,40 +22,15 @@ public class PlayerControls : MonoBehaviour
         transform.Translate(-Vector3.right * horizontalInput * speed * Time.deltaTime);
         transform.Translate(-Vector3.forward * verticalInput * speed * Time.deltaTime);
 
-        /*// Keep player in bounds
+    }
 
-        if (transform.position.x < -xRange)
+    public void BottomLimit()
+    {
+        // Limit bottom
+        if (transform.position.z < firstPersonBoundary)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y, firstPersonBoundary);
         }
-        else if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-
-        // Limit left side
-        if (transform.position.z < leftBoundary)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, leftBoundary);
-        }
-        // Limit right side
-        else if (transform.position.z > rightBoundary)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, rightBoundary);
-        }*/
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
 
