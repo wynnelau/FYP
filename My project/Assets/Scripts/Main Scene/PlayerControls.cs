9e6c46927c2d34b5c7bf29e3ce8aaf126a14dae5
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2023-09-05 12:11:46 +0800

    updated firstpersoncamera
    firstperson camera is now a child of player

--- My project/Assets/Scripts/Main Scene/PlayerControls.cs
+++ My project/Assets/Scripts/Main Scene/PlayerControls.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    public Camera firstPersonCamera;
     private float horizontalInput;
     private float verticalInput;
-    private float speed = 10.0f;
-    private Vector3 offset;
+    private float translateSpeed = 10.0f;
+
     private float firstPersonBoundary = -22.5f;
+
+    private float leftRotationInput;
+    private float rightRotationInput;
+    private float rotationSpeed = 2.5f;
+    private float rotateRight = -90f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +23,40 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Allow player to move around
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(-Vector3.right * horizontalInput * speed * Time.deltaTime);
-        transform.Translate(-Vector3.forward * verticalInput * speed * Time.deltaTime);
+        transform.Translate(-Vector3.right * horizontalInput * translateSpeed * Time.deltaTime);
+        transform.Translate(-Vector3.forward * verticalInput * translateSpeed * Time.deltaTime);
 
+        // Allow player to look left and right
+        if (firstPersonCamera.enabled)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                TurnLeft();
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                TurnRight();
+            }
+        }
+        
+    }        
+
+
+    void TurnLeft()
+    {
+        leftRotationInput = -rotateRight;
+        Quaternion rotationTargetLeft = Quaternion.Euler(0, leftRotationInput, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationTargetLeft, rotationSpeed * Time.deltaTime);
+    }
+
+    void TurnRight()
+    {
+        rightRotationInput = rotateRight;
+        Quaternion rotationTargetRight = Quaternion.Euler(0, rightRotationInput, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationTargetRight, rotationSpeed * Time.deltaTime);
     }
 
     public void BottomLimit()
@@ -32,5 +67,7 @@ public class PlayerControls : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, firstPersonBoundary);
         }
     }
+
+    
 }
 
