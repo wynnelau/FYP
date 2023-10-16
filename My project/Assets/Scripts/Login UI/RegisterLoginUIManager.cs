63193489a56bf667c285b07a2c922b79439640ea
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2023-10-16 22:17:13 +0800

    changed variable names

--- My project/Assets/Scripts/Login UI/RegisterLoginUIManager.cs
+++ My project/Assets/Scripts/Login UI/RegisterLoginUIManager.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 using TMPro;
 
 /*
- * Location: Login UI, used by UIControls
- * Purpose: Manage the register and login UI pages
+ * Location: UIControls
+ * Purpose: Manage the "RegisterPage" UI, "LoginPage" UI, "SetUserProfilePageStudent" UI and "SetUserProfilePageOthers" UI
  * Tutorial used: https://www.youtube.com/watch?v=XPTPRaF2pd4
  */
 public class RegisterLoginUIManager : MonoBehaviour
 {
-    public GameObject registerPage, loginPage, setUserProfileDataPageStudent, setUserProfileDataPageOthers;
-    // Cannot clear input field when declared as Text or TMP_Text
+    public GameObject registerPageUI, loginPageUI, setUserProfilePageStudentUI, setUserProfileDataOthersUI;
     public TMP_InputField registerEmail, registerPassword, loginEmail, loginPassword;
     public TMP_Dropdown identityDropdown;
     public TMP_Text registerError, loginError;
@@ -35,10 +34,10 @@ public class RegisterLoginUIManager : MonoBehaviour
      */
     public void ChangeToLogin()
     {
-        loginPage.SetActive(true);
-        registerPage.SetActive(false);
-        setUserProfileDataPageStudent.SetActive(false);
-        setUserProfileDataPageOthers.SetActive(false);
+        loginPageUI.SetActive(true);
+        registerPageUI.SetActive(false);
+        setUserProfilePageStudentUI.SetActive(false);
+        setUserProfileDataOthersUI.SetActive(false);
         loginEmail.text = "";
         loginPassword.text = "";
         loginError.text = " ";
@@ -50,10 +49,10 @@ public class RegisterLoginUIManager : MonoBehaviour
      */
     public void ChangeToRegister()
     {
-        loginPage.SetActive(false);
-        registerPage.SetActive(true);
-        setUserProfileDataPageStudent.SetActive(false);
-        setUserProfileDataPageOthers.SetActive(false);
+        loginPageUI.SetActive(false);
+        registerPageUI.SetActive(true);
+        setUserProfilePageStudentUI.SetActive(false);
+        setUserProfileDataOthersUI.SetActive(false);
         registerEmail.text = "";
         registerPassword.text = "";
         registerError.text = " ";
