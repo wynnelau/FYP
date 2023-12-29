95a4106dea64fb57f6decb044752695b862d282e
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2023-12-29 13:51:44 +0800

    finetune newMeetingUI
    also added to save list of email

--- My project/Assets/Scripts/Main Scene/NewMeetingUI.cs
+++ My project/Assets/Scripts/Main Scene/NewMeetingUI.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Text;
 using System;
+using System.Collections.Generic;
 
 public class NewMeetingUI : MonoBehaviour
 {
     public GameObject NewMeeting, MeetingSchedule;
-    public InputField participantEmail;
+    public InputField participantEmail, dDate, dMonth, dYear, duration, description;
+    public Text errorText, participantList;
+    public Dropdown tHr, tMin, tAM;
+
+    private List<string> emailList = new List<string>();
 
     /*
      * Purpose: To close the NewMeetingUI
@@ -18,8 +23,25 @@ public class NewMeetingUI : MonoBehaviour
     public void closeNewMeeting()
     {
         Debug.Log("NewMeetingUI closeNewMeeting");
-        NewMeeting.SetActive(true);
-        MeetingSchedule.SetActive(false);
+
+        errorText.text = "";
+        participantList.text = "";
+
+        participantEmail.text = "";
+        dDate.text = "";
+        dMonth.text = "";
+        dYear.text = "";
+        duration.text = "";
+        description.text = "";
+
+        tHr.value = 0;
+        tMin.value = 0;
+        tAM.value = 0;
+
+        emailList.Clear();
+
+        NewMeeting.SetActive(false);
+        MeetingSchedule.SetActive(true);
     }
 
     /*
@@ -27,9 +49,9 @@ public class NewMeetingUI : MonoBehaviour
      * Input: Click on AddUser button
      * Output: Check whether email/ user exists using email
      */
-    public void addParticipant()
+    public void AddParticipant()
     {
-        Debug.Log("NewMeetingUI addParticipant " + participantEmail.text);
+        Debug.Log("NewMeetingUI AddParticipant " + participantEmail.text);
         // Conversion required here as email in PlayFab database contains \xe2\x80\x8b at the end
         string email = StringToHex(participantEmail.text);
         string convertedEmail = HexToString(email + "e2808b");
@@ -37,14 +59,38 @@ public class NewMeetingUI : MonoBehaviour
         PlayFabClientAPI.GetAccountInfo(request, AddParticipantSuccess, AddParticipantFail);
     }
 
+    /*
+     * Purpose: Add the email to a list if the email/ user exists
+     * Input: Email/ user exists
+     * Output: Add email to list of emails or if already in list, return message
+     */
     void AddParticipantSuccess(GetAccountInfoResult result)
     {
-        Debug.Log(result.AccountInfo.PlayFabId);    
+        Debug.Log("NewMeetingUI AddParticipantSuccess");
+        if (emailList.Contains(participantEmail.text))
+        {
+            errorText.text = participantEmail.text + " already added";
+        } 
+        else
+        {
+            emailList.Add(participantEmail.text);
+            errorText.text = participantEmail.text + " successfully added";
+            participantList.text += participantEmail.text + "; ";
+            participantEmail.text = "";
+        }
+        
+        
     }
 
+    /*
+     * Purpose: Return an error message if email/ user does not exist
+     * Input: Email/ user does not exists
+     * Output: Return an error message
+     */
     void AddParticipantFail(PlayFabError error)
     {
-        Debug.LogError(error.GenerateErrorReport()); 
+        Debug.LogError(error.GenerateErrorReport());
+        errorText.text = error.GenerateErrorReport();
     }
 
     /*
