5683ec6b67eecd1a1a29cfc3d9c6958c241abbda
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2024-01-01 17:46:08 +0800

    Update ReturnToMainScene.cs

--- My project/Assets/Scripts/ClassRoom Scene/ReturnToMainScene.cs
+++ My project/Assets/Scripts/ClassRoom Scene/ReturnToMainScene.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ReturnToMainScene : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,8 @@ public class ReturnToMainScene : MonoBehaviour
 
     public void ReturnToMain()
     {
+        Relay relay = GetComponent<Relay>();
+        relay.StopRelay();
         SceneManager.LoadScene("Main Scene");
     }
 }
