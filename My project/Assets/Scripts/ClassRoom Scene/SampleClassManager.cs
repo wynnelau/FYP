bef67b2b5cd9e4a4cd2feb7381f082dc977e6716
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2024-02-25 18:50:09 +0800

    test

--- My project/Assets/Scripts/ClassRoom Scene/SampleClassManager.cs
+++ My project/Assets/Scripts/ClassRoom Scene/SampleClassManager.cs
@@ -5,32 +5,30 @@ using Unity.Netcode;
 
 public class SampleClassManager : MonoBehaviour
 {
-    public GameObject classSamplePrefab;
+    public GameObject classSamplePrefab, classSample;
     public Button enableSampleClass, disableSampleClass;
     
     public void EnableSampleClass()
     {
         if (NetworkManager.Singleton.IsHost)
         {
-            GameObject classSample = Instantiate(classSamplePrefab);
+            classSample = Instantiate(classSamplePrefab);
             classSample.GetComponent<NetworkObject>().Spawn();
         }
 
-        
         enableSampleClass.gameObject.SetActive(false);
         disableSampleClass.gameObject.SetActive(true);
-        //SampleClassNetwork.Instance?.ToggleSampleClassServerRpc(true);
     }
 
     public void DisableSampleClass()
     {
         if (NetworkManager.Singleton.IsHost)
         {
-            Destroy(classSamplePrefab);
+            Destroy(classSample);
         }
+
         enableSampleClass.gameObject.SetActive(true);
         disableSampleClass.gameObject.SetActive(false);
-        //SampleClassNetwork.Instance?.ToggleSampleClassServerRpc(false);
     }
 
 }
