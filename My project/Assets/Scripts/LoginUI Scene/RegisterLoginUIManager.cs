4c50bdecb995de6d705578c38f9e0fa7a375bd6b
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2024-02-26 12:25:43 +0800

    added comments

--- My project/Assets/Scripts/LoginUI Scene/RegisterLoginUIManager.cs
+++ My project/Assets/Scripts/LoginUI Scene/RegisterLoginUIManager.cs
@@ -2,55 +2,56 @@ using UnityEngine;
 using TMPro;
 
 /*
- * Location: Login UI/ UIControls
+ * Location: LoginUI Scene/ LoginUIControls
  * Purpose: Manage the "RegisterPage" UI, "LoginPage" UI, "SetUserProfilePageStudent" UI and "SetUserProfilePageOthers" UI
- * Tutorial used: https://www.youtube.com/watch?v=XPTPRaF2pd4
  */
 public class RegisterLoginUIManager : MonoBehaviour
 {
-    public GameObject registerPageUI, loginPageUI, setUserProfilePageStudentUI, setUserProfileDataOthersUI;
+    public GameObject registerUI, loginUI, setUserProfileStudentUI, setUserProfileOthersUI;
     public TMP_InputField registerEmail, registerPassword, loginEmail, loginPassword;
     public TMP_Dropdown identityDropdown;
     public TMP_Text registerError, loginError;
 
     /*
-     * Purpose: Ensures that user sees "LoginPage" UI when application opens, and other UIs are inactive
+     * Purpose: Ensures that user sees "Login" UI when application opens, and other UIs are inactive
      *          Start is called before the first frame update
      * Input: NA
      * Output: Set all UIs to inactive and call ChangeToLogin()
      */
     void Start()
     {
-        loginPageUI.SetActive(false);
-        registerPageUI.SetActive(false);
-        setUserProfilePageStudentUI.SetActive(false);
-        setUserProfileDataOthersUI.SetActive(false);
+        loginUI.SetActive(false);
+        registerUI.SetActive(false);
+        setUserProfileStudentUI.SetActive(false);
+        setUserProfileOthersUI.SetActive(false);
         ChangeToLogin();
     }
 
     /*
-     * Purpose: Change to "LoginPage" UI when the "LoginHereButton" button in "RegisterPage" UI is clicked
-     * Input: Click the "LoginHereButton" button in "RegisterPage" UI
-     * Output: Opens "LoginPage" UI, clears previous texts for loginEmail, loginPassword and loginError
+     * On Click: LoginUI Scene/ LoginUISceneCanvas/ Register/ LogInHereButton
+     * Purpose: Change to "Login" UI when the "LogInHereButton" button in "Register" UI is clicked
+     * Input: Click the "LogInHereButton" button in "Register" UI
+     * Output: Opens "Login" UI, clears previous texts for loginEmail, loginPassword and loginError
      */
     public void ChangeToLogin()
     {
-        loginPageUI.SetActive(true);
-        registerPageUI.SetActive(false);
+        loginUI.SetActive(true);
+        registerUI.SetActive(false);
         loginEmail.text = "";
         loginPassword.text = "";
         loginError.text = " ";
     }
 
     /*
-     * Purpose: Change to "RegisterPage" UI when the "RegisterHereButton" button in "LoginPage" UI is clicked
-     * Input: Click the "RegisterHereButton" button in "LoginPage" UI
-     * Output: Opens "RegisterPage" UI, clears previous texts for registerEmail, registerPassword, registerError and sets idenityDropdown to 0
+     * On Click: LoginUI Scene/ LoginUISceneCanvas/ Login/ RegisterHereButton
+     * Purpose: Change to "Register" UI when the "RegisterHereButton" button in "Login" UI is clicked
+     * Input: Click the "RegisterHereButton" button in "Login" UI
+     * Output: Opens "Register" UI, clears previous texts for registerEmail, registerPassword, registerError and sets idenityDropdown to 0
      */
     public void ChangeToRegister()
     {
-        loginPageUI.SetActive(false);
-        registerPageUI.SetActive(true);
+        loginUI.SetActive(false);
+        registerUI.SetActive(true);
         registerEmail.text = "";
         registerPassword.text = "";
         registerError.text = " ";
