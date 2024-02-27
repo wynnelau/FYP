956a2ca58707ee394c6b489f7f01b09fb46a3e9a
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2024-02-27 10:50:03 +0800

    Revert "Revert "Added quiz 1""
    This reverts commit 201c22d8c3384154d845bef791d092c1d1eaf1ab.

--- My project/Assets/Scripts/ClassRoom Scene/SampleQuizUI.cs
+++ My project/Assets/Scripts/ClassRoom Scene/SampleQuizUI.cs
@@ -1,6 +1,8 @@
+using Palmmedia.ReportGenerator.Core.Common;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
  * Location: ClassRoom Scene/ ClassRoomSceneControls
@@ -10,6 +12,9 @@ using UnityEngine;
 public class SampleQuizUI : MonoBehaviour
 {
     public GameObject sampleQuizUI;
+    public List<GameObject> questions;
+    public Text questionNumber;
+    public Button nextQuestionBtn, prevQuestionBtn;
     public void CloseSampleQuizUI()
     {
         sampleQuizUI.SetActive(false);
@@ -18,5 +23,46 @@ public class SampleQuizUI : MonoBehaviour
     public void OpenSampleQuizUI()
     {
         sampleQuizUI.SetActive(true);
+    }
+
+    public void NextQuestion()
+    {
+        int curQuestionNumber = int.Parse(questionNumber.text);
+        
+        questions[curQuestionNumber].SetActive(true);
+        questions[curQuestionNumber-1].SetActive(false);
+
+        curQuestionNumber += 1;
+        questionNumber.text = curQuestionNumber.ToString();
+
+        if (curQuestionNumber == questions.Count)
+        {
+            nextQuestionBtn.gameObject.SetActive(false);
+        }
+        if (curQuestionNumber != 1)
+        {
+            prevQuestionBtn.gameObject.SetActive(true);
+        }
+    }
+
+    public void PrevQuestion()
+    {
+        int curQuestionNumber = int.Parse(questionNumber.text);
+        curQuestionNumber -= 1;
+
+        questions[curQuestionNumber].SetActive(false);
+        questions[curQuestionNumber - 1].SetActive(true);
+
+        questionNumber.text = curQuestionNumber.ToString();
+
+        if (curQuestionNumber == 1)
+        {
+            prevQuestionBtn.gameObject.SetActive(false);
+        }
+        if (curQuestionNumber != questions.Count)
+        {
+            nextQuestionBtn.gameObject.SetActive(true);
+        }
+
     }
 }
